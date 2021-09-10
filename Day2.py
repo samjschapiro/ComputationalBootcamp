37ab2c1fca2f3e2551276d0f4e360a02ee8ce455
samjschapiro <81483219+samueljschapiro@users.noreply.github.com>
2021-09-10 08:19:55 -0500

    Finished Day 2 Exercises

--- Day2.py
+++ Day2.py
@@ -56,3 +56,114 @@ def reverse(list):
 
 
 print(reverse([3, 192, 4910, 1]))
+
+# all exercise 2
+
+# exercise2_1
+print("skip")
+
+# exercise2_2
+birthdays = [
+    ('Connie', 'Sept 4',),
+    ('Sam', 'Oct 19'),
+    ('Stu', 'Jun 7'),
+    ('Sangbum', 'Nov 17')
+]
+
+print(sorted(birthdays)) # sorts by first entry in tuple
+
+birthdays_by_date = [
+    ('Sept 4', 'Connie'),
+    ('Oct 19', 'Sam'),
+    ('Jun 7', 'Stu'),
+    ('Nov 17', 'Sangbum')
+]
+
+print(sorted(birthdays_by_date))
+
+# exercise2_3
+numbers = [n for n in range(1, 11)]
+numberPairs = []
+
+index = 0
+while (index < len(numbers) - 1):
+    numberPairs.append("("+str(numbers[index])+", "+str(numbers[index+1])+")")
+    index += 1
+
+print(numberPairs)
+
+# exercise2_4
+def vectorSum(v1, v2):
+    return (v1[0] + v2[0], v1[1] + v2[1])
+
+print(vectorSum((3, 4), (5, -1)))
+
+# all exercise 3
+
+# exercise3_1
+days_of_the_week = {
+    'Mon' : 1,
+    'Tues' : 2,
+    'Weds' : 3,
+    'Thur' : 4,
+    'Fri' : 5,
+    'Sat' : 6,
+    'Sun': 7
+}
+
+days_of_the_week_reversed = {
+    1 : 'Mon',
+    2 : 'Tues',
+    3 : 'Weds',
+    4 : 'Thur',
+    5 : 'Fri',
+    6 : 'Sat',
+    7 : 'Sun'
+}
+
+print(days_of_the_week)
+print(days_of_the_week_reversed)
+
+# exercise3_2
+items = ['dog', 'pencil', 'fence', 'dog', 'apple', 'dog', 'dog', 'dog', 'pear', 'pencil', 'pear', 'pear']
+
+counts = dict()
+for i in items:
+    counts[i] = counts.get(i, 0) + 1
+
+print(counts)
+
+# exercise3_3
+
+tuples = counts.items()
+sorted_tuples = sorted(tuples, key = lambda x: x[1], reverse = True)
+
+print(sorted_tuples[0])
+print(sorted_tuples[1])
+
+# all exercise 4 
+
+# exercise4_1
+def symmetric_dif(s1, s2):
+    return (s1 - s2) | (s2 - s1)
+
+A = set([1, 2, 5, 7, 8, 9])
+B = set([2, 4, 5, 8, 10])
+
+print(symmetric_dif(A, B))
+
+# exercise4_2
+list = [1, 4, 5, 3, 6, 7, 5, 3, 1, 9, 3, 3, 4, 2]
+
+list_to_set = set(list)
+print(list_to_set)
+
+# exercise4_3
+def allowed_users(users, bad):
+    set_users = set(users)
+    return set_users - bad
+
+users = ['tom', 'pam', 'sasha', 'jj', 'que', 'jeff']
+bad = set(['jeff', 'sasha'])
+
+print(allowed_users(users, bad))
